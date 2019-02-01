6737870c3e84568f93c8811b8d161a698b50f7a1
faetos <faetos@yahoo.com>
2019-01-31 18:18:30 -0600

    fixing dns seed addresses

--- main.cpp
+++ main.cpp
@@ -397,9 +397,8 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-//static const string mainnet_seeds[] = {"dns0.lytixchain.org", "dns1.lytixchain.org", "dns2.lytixchain.org", "dns3.lytixchain.org", "dns4.lytixchain.org", "dns5.lytixchain.org", ""};
-static const string mainnet_seeds[] = {"dnsseed01.quravo.com",  "dnsseed02.quravo.com", "dnsseed03.quravo.com", "dnsseed04.quravo.com", ""};
-static const string testnet_seeds[] = {""};
+static const string mainnet_seeds[] = {"dns0.lytixchain.org", "dns1.lytixchain.org", "dns2.lytixchain.org", "dns3.lytixchain.org", "dns4.lytixchain.org", "dns5.lytixchain.org", ""};
+static const string testnet_seeds[] = {"dns0.lytixchain.org", "dns1.lytixchain.org", "dns2.lytixchain.org", "dns3.lytixchain.org", "dns4.lytixchain.org", "dns5.lytixchain.org", ""};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
